678389700b87192637fe61d3a0f5dc6395e7fb9a
Raquel <silva.raquel2903@gmail.com>
2025-09-01 20:12:48 -0300

    adicionando o pedido numa lista de pedidos no database

--- src/repository/Database.java
+++ src/repository/Database.java
@@ -3,10 +3,15 @@ package repository;
 import model.Order;
 import repository.OrderRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Database implements OrderRepository {
 
+    List<Order> orders = new ArrayList<>();
     @Override
     public void save(Order order) {
+        orders.add(order);
         System.out.println("Pedido salvo com sucesso!");
     }
 }
